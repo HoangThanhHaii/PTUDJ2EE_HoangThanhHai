2e1e7bdb8f9b1a78a111fee8d3fd1d36c82445fc
PTUDJ2EE_HoangThanhHai <hoangthanhhai362003@gmail.com>
2026-01-26 17:12:56 +0700

    add bai tap

---
+++ Bai2_HoangThanhHai/src/main/java/com/example/Bai2HoangThanhHaiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai2HoangThanhHaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai2HoangThanhHaiApplication.class, args);
+	}
+
+}
